2d16c4ea749ac3d57411cf11edfab12a18d3a3dd
Federico <federico.mollea@mercadolibre.com>
2020-06-01 20:00:43 -0300

    changes sonsresponse

--- app/src/main/java/com/mosh/songfinder/data/services/data/SongsResponse.kt
+++ app/src/main/java/com/mosh/songfinder/data/services/data/SongsResponse.kt
@@ -2,10 +2,11 @@ package com.mosh.songfinder.data.services.data
 
 
 import com.google.gson.annotations.SerializedName
+import com.mosh.songfinder.domain.Song
 
 data class SongsResponse(
     @SerializedName("resultCount")
     val resultCount: Int,
     @SerializedName("results")
-    val results: List<Result>
+    val songs: List<Song>
 )
