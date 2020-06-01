22b18e6df1524cc5199e73dd6bcb23f1f5fff81c
Federico <federico.mollea@mercadolibre.com>
2020-05-31 22:02:44 -0300

    implemented getSongs method in the vm

--- app/src/main/java/com/mosh/songfinder/presentation/viewmodels/SongViewModel.kt
+++ app/src/main/java/com/mosh/songfinder/presentation/viewmodels/SongViewModel.kt
@@ -1,20 +1,35 @@
 package com.mosh.songfinder.presentation.viewmodels
 
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.mosh.songfinder.data.repository.SongRepository
 import com.mosh.songfinder.presentation.viewmodels.coroutine.CoroutineContextProvider
 import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class SongViewModel(
     private val repository: SongRepository,
     private val contextProvider: CoroutineContextProvider
-) {
+) : ViewModel() {
     private val handler = CoroutineExceptionHandler { _, exception ->
         stateLiveData.value = SongViewState.Error(exception)
     }
 
     private val stateLiveData = MutableLiveData<SongViewState>()
     fun getStateLiveData() = stateLiveData
+
+    fun getSongsFromServer(term: String) {
+        stateLiveData.value = SongViewState.Loading
+        viewModelScope.launch(handler) {
+            val data = withContext(contextProvider.IO) {
+                repository.getSongsFromServer(term)
+            }
+            stateLiveData.value = SongViewState.Success(data)
+        }
+    }
+
 
     sealed class SongViewState {
         object Loading : SongViewState()
