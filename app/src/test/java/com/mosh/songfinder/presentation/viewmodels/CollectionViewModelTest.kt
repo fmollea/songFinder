88dc507ae3de9bb45d47bf0ae9980e211b0e31e1
Federico <federico.mollea@mercadolibre.com>
2020-06-08 00:55:13 -0300

    green to red - create collection view model test

---
+++ app/src/test/java/com/mosh/songfinder/presentation/viewmodels/CollectionViewModelTest.kt
@@ -0,0 +1,71 @@
+package com.mosh.songfinder.presentation.viewmodels
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.mosh.songfinder.utils.TestContextProvider
+import com.mosh.songfinder.utils.TestCoroutineRules
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
+import retrofit2.Response
+import java.lang.Error
+
+@ExperimentalCoroutinesApi
+@Suppress("UNCHECKED_CAST")
+class CollectionViewModelTest {
+
+    @get:Rule
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @get:Rule
+    val testCoroutineRule = TestCoroutineRules()
+
+    @Mock
+    private lateinit var repository: CollectionRepository
+
+    @Mock
+    private lateinit var viewStateObserver: Observer<CollectionViewState>
+
+    private lateinit var viewModel: CollectionViewModel
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        viewModel = CollectionViewModel(
+            repository,
+            TestContextProvider()
+        ).apply {
+            getStateLiveData.observerForEver(viewStateObserver)
+        }
+    }
+
+    @Test
+    fun `test get collections from server success`() {
+        testCoroutineRule.runBlockingTest {
+            val data =  Response.success(mock(CollectionResponse::class.java))
+            val idCollection = 1
+
+            `when`(repository.getCollectionFromServer(idCollection)).thenReturn(data)
+            viewModel.getCollectionsFromServer(idCollection)
+
+            verify(viewStateObserver).onChanged(CollectionStateView.Loading)
+            verify(viewStateObserver).onChanged(CollectionStateView.SuccessSong(data.body()))
+        }
+    }
+
+    @Test
+    fun `test get collections from server fail`() {
+        testCoroutineRule.runBlockingTest {
+            val error = Error()
+            `when`(repository.getCollectionsFromServer(idCollection)).thenThrow(error)
+            viewModel.getCollectionsFromServer(idCollection)
+
+            verify(viewStateObserver).onChanged(CollectionStateView.Loading)
+            verify(viewStateObserver).onChanged(CollectionStateView.Error(error))
+        }
+    }
+}
